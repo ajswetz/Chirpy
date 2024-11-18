bf656bff891833af826f3052ce19b3bd624b63c0
ajswetz <ajswetz@icloud.com>
2024-11-17 22:10:40 -0500

    Wrapping up Ch1 on Servers

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 
 func main() {
 
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	server := http.Server{
+	// Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
+	// Use a standard http.FileServer as the handler
+	// Use http.Dir to convert a filepath (in our case a dot: . which indicates the current directory) to a directory for the http.FileServer.
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
+
+	server := &http.Server{
 		Addr:    ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
 
 	fmt.Println("Starting Chirpy . . . ")
@@ -19,5 +25,13 @@ func main() {
 	if err != nil {
 		fmt.Printf("An error occurred: %v\n", err)
 	}
+
+}
+
+func readinessHandler(resWriter http.ResponseWriter, _ *http.Request) {
+
+	resWriter.Header().Add("Content-Type:", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+	resWriter.Write([]byte("OK"))
 
 }
